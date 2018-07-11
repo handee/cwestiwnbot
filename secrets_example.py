1b734da81c1a80704ade7db98c0467c598353c21
Hannah <hmd1@aber.ac.uk>
2018-07-11 11:58:57 +0100

    added example secrets.py file

---
+++ secrets_example.py
@@ -0,0 +1,7 @@
+# add information about your own twitter authentication
+# and then rename this file to be secrets.py
+
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
