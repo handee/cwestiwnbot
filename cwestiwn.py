701dc56a8a863fed5d04cc46a9c39180fea8c437
handee <hmd@hannahdee.eu>
2018-09-12 12:32:27 +0100

    updated to have random congratulations, delete asked questions

--- cwestiwn.py
+++ cwestiwn.py
@@ -7,13 +7,16 @@ class cwestiwn(object):
      openquestions=[]
      input_data=[] 
      cwa=[]
+     welldone=[]
      def __init__(self):
         with open('questions.tsv','r') as tsv:
             self.input_data=[line.strip().split('\t') for line in tsv]
-            print("read in the questions\n")
         for i in range(len(self.input_data)):
             a=ateb(self.input_data[i][0],self.input_data[i][1:])
             self.cwa.append(a)
+        with open('congrats.tsv') as corpus_file:
+            self.welldone= corpus_file.readlines()
+
 
   
      def tweeted_at(self,user,tweettext):
@@ -21,15 +24,15 @@ class cwestiwn(object):
         # set tweet to placeholder
         tweet="notset"
         # go through open questions to check if this might be a response
-        for q in self.openquestions:
-            if q[0] == user:
-               print(q[0])
-               print(user)
-               tweet=self.check_answer(user,tweettext,q) 
-        # it's not a response so ask a new question
-        if (tweet=="notset"):
+        delete=-1
+        for i in range(0, len(self.openquestions)):
+            if self.openquestions[i][0] == user: # that user has an open question
+               tweet=self.check_answer(user,tweettext,self.openquestions[i]) 
+               delete=i # keep track of which one you need to delete
+        if (delete >= 0) : # it was an answer so delete the question
+            del self.openquestions[delete] 
+        else : # it's not a response so ask a new question
             tweet=self.get_question(user)
-        print(tweet)
         return(tweet)
 
      def get_question(self,user):
@@ -39,48 +42,42 @@ class cwestiwn(object):
         q=self.cwa[r].que
         a=self.cwa[r].ans
         self.openquestions.append([user,a])
-	    
+        
         tweetstring="@"+user+" "+q;
         return(tweetstring)
 
      def check_answer(self,user,tweettext,a):
         correct=False
-        lower_answer=tweettext.lower()
-        match_string=lower_answer.translate(None, string.punctuation)
-        print(tweettext)
-        print(match_string)
-        print (a)
-        print (a[1])
-        print (len(a[1]))
-        for i in range(0,len(a[1])-1):
+        match_string=tweettext.lower()
+        match_string = match_string.translate({ord(c): None for c in string.punctuation})
+        for i in range(0,len(a[1])):
             if match_string==a[1][i].lower():
                correct=True
         if correct==True:
-            tweet="@"+user+" bendigedig!"
+            congrats=random.choice(self.welldone)
+            tweet="@"+user+" "+congrats
         else :
             tweet="@"+user+" na sori, yr ateb ywr "
             for i in range(0,len(a[1])-1):
-                print i
                 tweet+=a[1][i]
                 tweet+=", "
-            tweet+="neu "
-	    tweet+=a[1][-1]
-        print(tweet)
+                tweet+="neu "
+                tweet+=a[1][-1]
         return(tweet)
 
      def test_q_dataset(self):
         for i in range(0,len(self.cwa)):
-             print "question"
-             print self.cwa[i].que
-             print "answers"
-             print self.cwa[i].ans
+             print("question")
+             print(self.cwa[i].que)
+             print("answers")
+             print(self.cwa[i].ans)
 
 
 
      def test_q_buffer(self):
          print(self.get_question("user0"))
-         print self.openquestions 
+         print(self.openquestions )
          print(self.get_question("user1"))
-         print self.openquestions 
+         print(self.openquestions )
          print(self.get_question("user2"))
-         print self.openquestions 
+         print(self.openquestions )
